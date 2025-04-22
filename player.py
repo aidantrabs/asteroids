8d16430ce4d8d7541c1bd634a228d3a2327a6a01
Aidan Traboulay <aidantraboulay13@gmail.com>
2025-04-22 04:18:26 -0400

    call triangle correctly

--- player.py
+++ player.py
@@ -24,4 +24,4 @@ class Player(CircleShape):
         return [a, b, c]
     
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle, 2)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
